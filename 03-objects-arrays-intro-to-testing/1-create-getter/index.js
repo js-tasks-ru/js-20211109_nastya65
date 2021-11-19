76fa1d8a6c451c56a8cd40f561ffeba3ec515537
pashkevich.al <pashkevich.al@esphere.ru>
2021-11-19 19:58:19 +0300

    Выполнены задачи по третьему уроку

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,19 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const getter = function (obj) {
+    const pathArray = path.split('.');
+    let newObj = obj;
 
+    pathArray.forEach(item => {
+      if (!newObj) {
+        return undefined;
+      }
+      newObj = newObj[item];
+    })
+
+    return newObj;
+  };
+
+  return getter;
 }
